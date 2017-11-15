b7a9472540ff4d347984dd085813493af31db5e3
Robert Jameson <jamesonr@uni.coventry.ac.uk>
2017-11-15 09:40:31 +0000

    Raw data now showing on browser

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(function(req,res,next){
     next();
 
 })
+
+app.get('/api/message', GetMessages);
 app.post('/api/message', function(req,res){
     console.log(req.body);
     var message = new Message(req.body);
@@ -22,17 +24,16 @@ app.post('/api/message', function(req,res){
     res.status(200);
 })
 
-function GetMessages()
+function GetMessages(req, res)
 {
     Message.find({}).exec(function(err, result){
-        console.log(result);
+        res.send(result);
     })
 }
 
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("connected to the db");
-        GetMessages();
     }
 })
 var server = app.listen(5000, function(){
