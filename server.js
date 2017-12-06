3c63ff9324ce70c12b151d89b4a86e3b6c675b42
Robert Jameson <jamesonr@uni.coventry.ac.uk>
2017-12-06 19:16:34 +0000

    Usertoken, email & message connected

--- server.js
+++ server.js
@@ -2,35 +2,30 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-
-})
-
+//Requests
 app.get('/api/message', message.get);
 
-app.post('/api/message', message.post);
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
-mongoose.connect("mongodb://localhost:27017/test", function(err,db){
-    if(!err){
-        console.log("connected to the db");
+//Connection
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
+        console.log("we are connected to mongo");
     }
 })
-var server = app.listen(5000, function(){
-    console.log ('listening on port', server.address().port);
-    })
 
-
-
-
+var server = app.listen(5000, function () {
+    console.log('listening on port ', server.address().port)
+})
 
